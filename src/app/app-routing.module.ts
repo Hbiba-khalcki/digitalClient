eb3721f9044defb6e3a2acf5d4b6f28cf595d829
hbiba-khalcki <khalckikhalcki@gmail.com>
2021-04-17 23:33:48 +0100

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { InfoClientComponent } from './components/client/renseignements/info-client/info-client.component';
 import { OrganisationTypeComponent } from './components/client/renseignements/organisation-type/organisation-type.component';
+import { FirstComponentComponent } from './components/client/strategie-leadership/first-component/first-component.component';
 import { HomeComponent } from './components/shared/home/home.component';
 
 const routes: Routes = [
   {path:'home', component:HomeComponent},
   {path:'typeorganisation', component:OrganisationTypeComponent},
-  {path:'infoclient', component:InfoClientComponent}
+  {path:'infoclient', component:InfoClientComponent},
+  {path:'sectionOne', component:FirstComponentComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,18 +10,22 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule , FormsModule } from '@angular/forms';
 import { NavBarComponent } from './components/shared/nav-bar/nav-bar.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './components/shared/home/home.component';
 import { OrganisationTypeComponent } from'./components/client/renseignements/organisation-type/organisation-type.component';
 import { InfoClientComponent } from './components/client/renseignements/info-client/info-client.component';
-
+import {MatInputModule} from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { FirstComponentComponent } from './components/client/strategie-leadership/first-component/first-component.component';
 @NgModule({
   declarations: [
     AppComponent, 
     NavBarComponent, 
     HomeComponent, 
-    OrganisationTypeComponent, InfoClientComponent
+    OrganisationTypeComponent, InfoClientComponent, FirstComponentComponent
   ],
   imports: [
     BrowserModule,
@@ -33,8 +37,13 @@ import { InfoClientComponent } from './components/client/renseignements/info-cli
     MatButtonModule,
     FlexLayoutModule,
     MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
     AppRoutingModule,
-    MatFormFieldModule
+    FormsModule,
+    ReactiveFormsModule,
+    MatRadioModule,
+    MatProgressBarModule
   ],
   exports: [
     MatToolbarModule,
