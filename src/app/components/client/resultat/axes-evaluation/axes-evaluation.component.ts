f79a50eed7cacf698219b67326cf82da9ee7a2c0
hbiba-khalcki <khalckikhalcki@gmail.com>
2021-09-20 13:07:25 +0100

    generer pdf

--- src/app/components/client/resultat/axes-evaluation/axes-evaluation.component.ts
+++ src/app/components/client/resultat/axes-evaluation/axes-evaluation.component.ts
@@ -4,6 +4,7 @@ import { ProgressSpinnerMode } from '@angular/material/progress-spinner';
 import { Router } from '@angular/router';
 import { AxeService } from 'src/app/services/axe.service';
 import { ExpertService } from 'src/app/services/expert.service';
+import { jsPDF } from "jspdf";
 
 @Component({
   selector: 'app-axes-evaluation',
@@ -26,7 +27,6 @@ export class AxesEvaluationComponent implements OnInit {
     private axeService: AxeService, 
     private router:Router
   ) {}
-
   ngOnInit(): void {
     this.axeService.getAllAxes().subscribe((data) => {
       this.axes = data;
@@ -40,5 +40,63 @@ export class AxesEvaluationComponent implements OnInit {
   }
   round2decimals(a: any) {
     return isNaN(a)?0:Math.round(a * 100) / 100;
-  }
+  } 
+  
+generatepdf(){
+  const doc = new jsPDF('p', 'px', 'a4');
+  let html =`
+  <html>
+    <body>
+        <h2 class="h2">rapport de maturite</h2>
+        `
+  html+=`
+        <div class="row c1"><div class="title" >strategie et leadership </div><div class="percent">20%</div></div>
+        <div class="row c2"><div class="title" >technologie et services </div><div class="percent">20%</div></div>
+        <div class="row c3"><div class="title" >culture et innovation </div><div class="percent">20%</div></div>
+        <div class="row c4"><div class="title" >operation et processus </div><div class="percent">20%</div></div>
+        <div class="row c5"><div class="title" >infrastructure et information </div><div class="percent">20%</div></div>
+        `
+        
+  html+=`
+         <style>
+             body{
+                 margin:0;
+                 padding: 0;
+             }
+             .title{
+                display: inline;
+                font-size:12px;
+                width: 300px;
+             }
+             .h2{
+               font-size:18px;
+               text-align:center;
+               width:500px
+             }
+             .row{
+                  padding: 10px 20px;
+                  width:400px;
+                  color:#333;
+                  height:30px;
+             }
+             .percent{
+                 display: inline;
+                 width: 100px;
+                 font-size:10px;
+             }
+             .c1 ,.c3 ,.c5{
+                 background-color: #ddd;
+             }
+             .c2 ,.c4 {
+                 background-color: #bbb;
+             }
+             
+         </style>
+     </body>
+</html>
+`;
+  doc.html(html,{margin:1,autoPaging:true}).then(()=>
+    doc.save("rapport.pdf")
+  )
+}
 }
